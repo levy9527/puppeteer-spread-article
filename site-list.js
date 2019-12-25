5a761016324f982b785ad71677f1d1ddfe183b30
levy <897895407@qq.com>
2019-12-25 18:00:49 +0800

    feat: hacpai login

--- site-list.js
+++ site-list.js
@@ -10,36 +10,52 @@
 module.exports = [
   // 登录状态保持时间较短, 有时可能需要重新登录两次
   {
-    //url: 'https://hacpai.com/post?type=0',
-    selectorTitle: "#articleTitle",
-    selectorContent: ".vditor-textarea",
+    url: 'https://hacpai.com/post?type=0',
+    selectorTitle: '#articleTitle',
+    selectorContent: '.vditor-textarea',
+    beforeWriting: async page => {
+      const btnLogin = '.navLogin'
+      const btnGithub = '.verifyOauthBtn'
+
+      try {
+        await page.waitForSelector(btnLogin, {timeout: 500})
+        await page.click(btnLogin)
+
+        await page.waitForSelector(btnLogin, {timeout: 500})
+        await page.click(btnGithub)
+
+        await page.waitFor(8000)
+      } catch (e) {
+        console.log('hacpai no need to login')
+      }
+    },
     extraOperations: async page => {
-      await page.click("#showReward");
+      await page.click('#showReward')
     }
   },
   {
     //url: 'https://forum.vuejs.org/c/chinese',
-    selectorTitle: "#reply-title",
-    selectorContent: ".ember-text-area",
+    selectorTitle: '#reply-title',
+    selectorContent: '.ember-text-area',
     beforeWriting: async page => {
-      const btnLogin = ".login-button";
-      const btnGithub = ".btn-social.github";
+      const btnLogin = '.login-button'
+      const btnGithub = '.btn-social.github'
 
       try {
         // 登录
-        await page.waitForSelector(btnLogin, { timeout: 100 });
-        await page.click(btnLogin);
+        await page.waitForSelector(btnLogin, {timeout: 100})
+        await page.click(btnLogin)
 
         // 使用 github 账号
-        await page.waitForSelector(btnGithub, { timeout: 500 });
-        await page.click(btnGithub);
+        await page.waitForSelector(btnGithub, {timeout: 500})
+        await page.click(btnGithub)
 
-        await page.waitFor(3000);
+        await page.waitFor(3000)
       } catch (e) {
-        console.log("forum.vuejs: no need to login");
+        console.log('forum.vuejs: no need to login')
       } finally {
-        await page.click("#create-topic");
-        await page.waitFor(500);
+        await page.click('#create-topic')
+        await page.waitFor(500)
 
         //await page.click('.toggle-fullscreen')
       }
@@ -48,56 +64,56 @@ module.exports = [
   // 有时图片会上传失败
   {
     //url: 'https://github.com/levy9527/blog/issues/new',
-    selectorTitle: "#issue_title",
-    selectorContent: "#issue_body"
+    selectorTitle: '#issue_title',
+    selectorContent: '#issue_body'
   },
   {
     //url: 'https://segmentfault.com/write?freshman=1',
-    selectorTitle: "#myTitle",
-    selectorContent: "#myEditor"
+    selectorTitle: '#myTitle',
+    selectorContent: '#myEditor'
   },
   {
     //url: 'https://my.oschina.net/u/2414419/blog/write',
-    selectorTitle: "input[name=title]",
-    selectorContent: ".CodeMirror-wrap textarea",
+    selectorTitle: 'input[name=title]',
+    selectorContent: '.CodeMirror-wrap textarea',
     extraOperations: async page => {
-      await page.click("#bodyEditorWrap + .fields .selection");
-      await page.waitForSelector(".selection.visible .menu.visible .item");
-      await page.click(".selection.visible .menu.visible .item:nth-child(2)");
+      await page.click('#bodyEditorWrap + .fields .selection')
+      await page.waitForSelector('.selection.visible .menu.visible .item')
+      await page.click('.selection.visible .menu.visible .item:nth-child(2)')
     }
   },
   {
     //url: 'https://www.v2ex.com/new/blog',
-    selectorTitle: "#topic_title",
-    selectorContent: "#topic_content",
+    selectorTitle: '#topic_title',
+    selectorContent: '#topic_content',
     extraOperations: async page => {
-      await page.click("#select_syntaxSelectBoxIt");
-      await page.waitForSelector("#select_syntaxSelectBoxItOptions");
-      await page.click(".selectboxit-option-last");
+      await page.click('#select_syntaxSelectBoxIt')
+      await page.waitForSelector('#select_syntaxSelectBoxItOptions')
+      await page.click('.selectboxit-option-last')
     }
   },
   {
     //url: 'https://www.jianshu.com/writer#/notebooks/26371741/notes/50149419',
-    selectorTitle: "._24i7u",
-    selectorContent: "#arthur-editor"
+    selectorTitle: '._24i7u',
+    selectorContent: '#arthur-editor'
   },
   // 无法预览
   {
     //url: 'https://i.cnblogs.com/EditPosts.aspx?opt=1',
-    selectorTitle: "#Editor_Edit_txbTitle",
-    selectorContent: "#Editor_Edit_EditorBody",
+    selectorTitle: '#Editor_Edit_txbTitle',
+    selectorContent: '#Editor_Edit_EditorBody',
     extraOperations: async page => {
-      await page.click("#Editor_Edit_APOptions_Advancedpanel1_cklCategories_0");
+      await page.click('#Editor_Edit_APOptions_Advancedpanel1_cklCategories_0')
       await page.click(
-        "#Editor_Edit_APOptions_APSiteHome_cbIsPublishToSiteHome"
-      );
+        '#Editor_Edit_APOptions_APSiteHome_cbIsPublishToSiteHome'
+      )
     }
   },
   // 登录状态保持时间较短, 有时可能需要重新登录两次
   {
     //url: 'https://mp.csdn.net/mdeditor?not_checkout=0#',
-    selectorTitle: ".article-bar__title--input",
-    selectorContent: ".editor__inner",
+    selectorTitle: '.article-bar__title--input',
+    selectorContent: '.editor__inner',
     extraOperations: async page => {
       // 前几次会出现markdown-editor的提示
       //const selectorHand = '.middle-hand > button'
@@ -108,26 +124,26 @@ module.exports = [
   // 登录状态保持时间较短, 有时可能需要重新登录两次
   {
     //url: 'https://blog.51cto.com/blogger/publish',
-    selectorTitle: "#title",
-    selectorContent: ".CodeMirror > div > textarea",
+    selectorTitle: '#title',
+    selectorContent: '.CodeMirror > div > textarea',
     extraOperations: async page => {
-      await page.waitFor(100);
+      await page.waitFor(100)
 
       // 选择原创
-      await page.hover(".pulldown-title");
-      await page.waitForSelector(".pulldown-title.on");
-      await page.click(".pulldown-title .pulldown-list li:first-child");
+      await page.hover('.pulldown-title')
+      await page.waitForSelector('.pulldown-title.on')
+      await page.click('.pulldown-title .pulldown-list li:first-child')
 
       // 选择一级分类
-      await page.hover(".system-one");
-      await page.waitForSelector(".system-one.on");
-      await page.click("#system-one-list li:nth-child(5)");
+      await page.hover('.system-one')
+      await page.waitForSelector('.system-one.on')
+      await page.click('#system-one-list li:nth-child(5)')
 
       // 选择二级分类
-      await page.waitFor(1000);
-      await page.hover(".system-two");
-      await page.waitForSelector(".system-two.on");
-      await page.click("#system-two-list li:nth-child(5)");
+      await page.waitFor(1000)
+      await page.hover('.system-two')
+      await page.waitForSelector('.system-two.on')
+      await page.click('#system-two-list li:nth-child(5)')
 
       // 以防外一：出现按钮看不见的情况
       // await page.click('#submit')
@@ -135,35 +151,35 @@ module.exports = [
   },
   // 登录状态保持时间较短
   {
-    url: "https://juejin.im",
-    selectorTitle: ".title-input",
-    selectorContent: ".ace_text-input",
+    //url: "https://juejin.im",
+    selectorTitle: '.title-input',
+    selectorContent: '.ace_text-input',
     beforeWriting: async page => {
       try {
-        const btnLogin = ".login";
-        const btnGithub = ".oauth-bg:last-child .oauth-btn";
+        const btnLogin = '.login'
+        const btnGithub = '.oauth-bg:last-child .oauth-btn'
 
-        await page.waitForSelector(btnLogin, { timeout: 1000 });
-        await page.click(btnLogin);
-        await page.click(btnGithub);
+        await page.waitForSelector(btnLogin, {timeout: 1000})
+        await page.click(btnLogin)
+        await page.click(btnGithub)
 
-        await page.waitFor(5000);
+        await page.waitFor(8000)
       } catch (e) {
-        console.log("juejin no need to login");
+        console.log('juejin no need to login')
       } finally {
-        await page.click(".add-btn");
-        await page.waitFor(3000);
+        await page.click('.add-btn')
+        await page.waitFor(3000)
       }
     }
   },
   {
     //url: 'https://zhuanlan.zhihu.com/write',
-    selectorTitle: ".WriteIndex-titleInput .Input",
-    selectorContent: ".public-DraftEditor-content"
+    selectorTitle: '.WriteIndex-titleInput .Input',
+    selectorContent: '.public-DraftEditor-content'
   },
   {
     //url: 'https://medium.com/new-story',
-    selectorTitle: ".graf--title",
-    selectorContent: ".section-inner > p"
+    selectorTitle: '.graf--title',
+    selectorContent: '.section-inner > p'
   }
-];
+]
